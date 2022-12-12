25b0c21bfe9386b8bcbe243e98182657f59d48f1
mfsaglam <mfsaglam1@gmail.com>
2022-12-12 15:50:47 +0300

    add feedLoader load method

--- EssentialFeed/FeedLoader.swift
+++ EssentialFeed/FeedLoader.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+enum LoadFeedResult {
+    case success([FeedItem])
+    case failure(Error)
+}
+
 protocol FeedLoader {
-    
+    func load(completion: @escaping (LoadFeedResult) -> Void )
 }
