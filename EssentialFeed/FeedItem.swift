0e38eebee0335ee2fe57f2d47219b33cc49ed19e
mfsaglam <mfsaglam1@gmail.com>
2022-12-12 15:46:04 +0300

    add feedItem and FeedLoader

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Fatih Sağlam on 12.12.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Fatih Sağlam on 12.12.2022.
+//
+
+import Foundation
+
+protocol FeedLoader {
+    
+}
