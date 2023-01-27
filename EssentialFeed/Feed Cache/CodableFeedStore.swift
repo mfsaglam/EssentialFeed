935c9e0d1d8af26b54b2330d1b45648729e629f5
mfsaglam <mfsaglam1@gmail.com>
2023-01-27 16:19:47 +0300

    Move `CodableFeedStore` to its own file in production

---
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -0,0 +1,83 @@
+//
+//  CodableFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Fatih Sağlam on 27.01.2023.
+//
+
+import Foundation
+
+public class CodableFeedStore: FeedStore {
+    
+    private struct Cache: Codable {
+        let images: [CodableFeedImage]
+        let timestamp: Date
+        
+        var localFeed: [LocalFeedImage] {
+            return images.map { $0.local }
+        }
+    }
+    
+    private struct CodableFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(_ image: LocalFeedImage) {
+            self.id = image.id
+            self.description = image.description
+            self.location = image.location
+            self.url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            LocalFeedImage(id: self.id, description: self.description, location: self.location, url: self.url)
+        }
+    }
+    
+    let storeUrl: URL
+    
+    public init(storeUrl: URL) {
+        self.storeUrl = storeUrl
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(images: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encodedFeed = try! encoder.encode(cache)
+            try encodedFeed.write(to: storeUrl)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        guard let data = try? Data(contentsOf: storeUrl) else {
+            return completion(.empty)
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        guard FileManager.default.fileExists(atPath: storeUrl.path) else {
+            return completion(nil)
+        }
+        
+        do {
+            try FileManager.default.removeItem(at: storeUrl)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+}
