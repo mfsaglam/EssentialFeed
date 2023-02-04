a93876604dd7fe452e0128f877164cdd878b3098
mfsaglam <mfsaglam1@gmail.com>
2023-02-04 22:06:24 +0300

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` overrides items saved by separate instances, proving we correctly manage the data models on disk.

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -39,6 +39,30 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         expect(sutToPerformLoad, toLoad: feed)
     }
     
+    func test_save_overridesItemsSavedOnASeperateInstance() {
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().model
+        let latestFeed = uniqueImageFeed().model
+        
+        let saveExp1 = expectation(description: "Wait for save completion")
+        sutToPerformFirstSave.save(firstFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save successfully")
+            saveExp1.fulfill()
+        }
+        wait(for: [saveExp1], timeout: 1)
+        
+        let saveExp2 = expectation(description: "Wait for save completion")
+        sutToPerformLastSave.save(latestFeed) { saveError in
+            XCTAssertNil(saveError, "Expected to save successfully")
+            saveExp2.fulfill()
+        }
+        wait(for: [saveExp2], timeout: 1)
+        
+        expect(sutToPerformLoad, toLoad: latestFeed)
+    }
+    
     // MARK:- Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> LocalFeedLoader {
@@ -66,8 +90,6 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1)
     }
-    
-    
     
     private func setupEmptyStoreState() {
         deleteStoreSideEffects()
