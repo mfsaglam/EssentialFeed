fce795bf99d2d471b4f550892907c6fdd9d46024
mfsaglam <mfsaglam1@gmail.com>
2023-02-04 17:20:07 +0300

    Move `CoreData` helpers from `CoreDataFeedStore` into separate file.

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -5,7 +5,6 @@
 //  Created by Fatih Sağlam on 4.02.2023.
 //
 
-import Foundation
 import CoreData
 
 public class CoreDataFeedStore: FeedStore {
@@ -63,36 +62,6 @@ public class CoreDataFeedStore: FeedStore {
         context.perform { action(context) }
     }
 }
-
-private extension NSPersistentContainer {
-    private enum LoadError: Error {
-        case modelNotFound
-        case failedToLoadPersistentStore(Error)
-    }
-    
-    class func loadAndReturn(modelName: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-        guard let momd = NSManagedObjectModel.with(modelName: modelName, in: bundle)
-        else { throw LoadError.modelNotFound }
-        
-        let description = NSPersistentStoreDescription.init(url: url)
-        let container = NSPersistentContainer.init(name: "FeedStore", managedObjectModel: momd)
-        container.persistentStoreDescriptions = [description]
-
-        var loadError: Error?
-        container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw LoadError.failedToLoadPersistentStore($0) }
-        
-        return container
-    }
-}
-
- private extension NSManagedObjectModel {
-     class func with(modelName: String, in bundle: Bundle) -> NSManagedObjectModel? {
-         return bundle
-             .url(forResource: modelName, withExtension: "momd")
-             .flatMap { NSManagedObjectModel.init(contentsOf: $0) }
-     }
- }
 
 @objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
