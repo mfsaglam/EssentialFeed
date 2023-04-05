6dd0f5ebd80eab16ddaa20ce468e40fdb68ae8ea
mfsaglam <mfsaglam1@gmail.com>
2023-04-05 17:12:04 +0300

    Move FeedViewModel into its own file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Fatih Sağlam on 5.04.2023.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
